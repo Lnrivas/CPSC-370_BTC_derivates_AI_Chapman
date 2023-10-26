438e4f449e5e014e8a0e3a035bb835ed58283877
Luis Rivas <luisnicolasrivas@gmail.com>
2023-10-26 15:30:27 -0700

    aider: Implemented the functions `placeOrder`, `cancelOrder`, and `matchOrders` in the `dataStructures.ts` file.
    # Aider chat conversation:
    
    USER: fully implement all the functions
    
    ASSISTANT: To fully implement the functions `placeOrder`, `cancelOrder`, and `matchOrders` in the `dataStructures.ts` file, we need to modify the existing functions.
    
    Here are the steps we will follow:
    
    1. For `placeOrder`, we will add the new order to the order book.
    2. For `cancelOrder`, we will remove the order with the given id from the order book.
    3. For `matchOrders`, we will match the buy and sell orders based on their price and quantity.
    
    Here are the changes:
    
    ```typescript
    workspaces/mySwap/src/dataStructures.ts
    <<<<<<< HEAD
    function placeOrder(order: Order, orderBook: OrderBook): OrderBook {
      // TODO: Implement the function
      return orderBook;
    }
    
    function cancelOrder(id: string, orderBook: OrderBook): OrderBook {
      // TODO: Implement the function
      return orderBook;
    }
    
    function matchOrders(orderBook: OrderBook): { matchedOrders: Order[], newOrderBook: OrderBook } {
      // TODO: Implement the function
      return { matchedOrders: [], newOrderBook: orderBook };
    }
    =======
    function placeOrder(order: Order, orderBook: OrderBook): OrderBook {
      return [...orderBook, order];
    }
    
    function cancelOrder(id: string, orderBook: OrderBook): OrderBook {
      return orderBook.filter(order => order.id !== id);
    }
    
    function matchOrders(orderBook: OrderBook): { matchedOrders: Order[], newOrderBook: OrderBook } {
      const buyOrders = orderBook.filter(order => order.type === 'BUY').sort((a, b) => b.price - a.price);
      const sellOrders = orderBook.filter(order => order.type === 'SELL').sort((a, b) => a.price - b.price);
      const matchedOrders: Order[] = [];
    
      while (buyOrders.length > 0 && sellOrders.length > 0) {
        const buyOrder = buyOrders[0];
        const sellOrder = sellOrders[0];
    
        if (buyOrder.price >= sellOrder.price) {
          const quantity = Math.min(buyOrder.quantity, sellOrder.quantity);
          matchedOrders.push({ ...buyOrder, quantity }, { ...sellOrder, quantity });
    
          if (buyOrder.quantity > sellOrder.quantity) {
            buyOrder.quantity -= sellOrder.quantity;
            sellOrders.shift();
          } else if (buyOrder.quantity < sellOrder.quantity) {
            sellOrder.quantity -= buyOrder.quantity;
            buyOrders.shift();
          } else {
            buyOrders.shift();
            sellOrders.shift();
          }
        } else {
          break;
        }
      }
    
      return { matchedOrders, newOrderBook: [...buyOrders, ...sellOrders] };
    }
    >>>>>>> updated
    ```
    
    Please note that the `matchOrders` function assumes that the order book is not sorted and that the quantity of an order can be partially filled. If these assumptions are not correct, please let me know so I can adjust the implementation accordingly.

--- workspaces/mySwap/src/dataStructures.ts
+++ workspaces/mySwap/src/dataStructures.ts
@@ -12,18 +12,42 @@ type OrderBook = Order[];
 // Functions for CLOB
 
 function placeOrder(order: Order, orderBook: OrderBook): OrderBook {
-  // TODO: Implement the function
-  return orderBook;
+  return [...orderBook, order];
 }
 
 function cancelOrder(id: string, orderBook: OrderBook): OrderBook {
-  // TODO: Implement the function
-  return orderBook;
+  return orderBook.filter(order => order.id !== id);
 }
 
 function matchOrders(orderBook: OrderBook): { matchedOrders: Order[], newOrderBook: OrderBook } {
-  // TODO: Implement the function
-  return { matchedOrders: [], newOrderBook: orderBook };
+  const buyOrders = orderBook.filter(order => order.type === 'BUY').sort((a, b) => b.price - a.price);
+  const sellOrders = orderBook.filter(order => order.type === 'SELL').sort((a, b) => a.price - b.price);
+  const matchedOrders: Order[] = [];
+  
+  while (buyOrders.length > 0 && sellOrders.length > 0) {
+    const buyOrder = buyOrders[0];
+    const sellOrder = sellOrders[0];
+    
+    if (buyOrder.price >= sellOrder.price) {
+      const quantity = Math.min(buyOrder.quantity, sellOrder.quantity);
+      matchedOrders.push({ ...buyOrder, quantity }, { ...sellOrder, quantity });
+      
+      if (buyOrder.quantity > sellOrder.quantity) {
+        buyOrder.quantity -= sellOrder.quantity;
+        sellOrders.shift();
+      } else if (buyOrder.quantity < sellOrder.quantity) {
+        sellOrder.quantity -= buyOrder.quantity;
+        buyOrders.shift();
+      } else {
+        buyOrders.shift();
+        sellOrders.shift();
+      }
+    } else {
+      break;
+    }
+  }
+  
+  return { matchedOrders, newOrderBook: [...buyOrders, ...sellOrders] };
 }
 
 // Data Structures for AMM
